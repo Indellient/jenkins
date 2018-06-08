90f44ab96187ce62479d01f872d6112f1fc029b1
skylerto <skylerclayne@gmail.com>
2018-06-08 14:54:24 -0400

    poc
    Signed-off-by: skylerto <skylerclayne@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Habitat package: jenkins
+
+## Description
+
+Provide a brief description of the `jenkins` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `jenkins` plan
---
+++ plan.sh
@@ -0,0 +1,27 @@
+pkg_name=jenkins
+pkg_origin=skylerto
+pkg_version=2.121.1
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
+pkg_license=('mit')
+pkg_upstream_url="https://jenkins.io/"
+pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
+pkg_shasum="5bb075b81a3929ceada4e960049e37df5f15a1e3cfc9dc24d749858e70b48919"
+pkg_deps=(core/jre8 core/curl)
+pkg_exports=(
+  [port]=jenkins.http.port
+)
+pkg_exposes=(port)
+pkg_svc_user="root"
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp "${HAB_CACHE_SRC_PATH}"/"${pkg_filename}" "${pkg_prefix}"/jenkins.war
+}
