f074930e49a0b9cec87a22d72a9701a65a5c8a3f
skylerto <skylerclayne@gmail.com>
2018-06-18 09:15:39 -0400

    restart jenkins after plugin installs
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- config/init.groovy
+++ config/init.groovy
@@ -120,45 +120,12 @@ if(missing_plugins.size() > 0) {
 
 if(hasConfigBeenUpdated) {
     println "Saving Jenkins configuration to disk."
-    Jenkins.instance.save()
+    instance.save()
+    instance.restart()
 } else {
     println "Jenkins up-to-date.  Nothing to do."
 }
 
+// Configure jobs? 
+
 instance.save()
-
-//user configurable variable
-if(!binding.hasVariable('timeout_seconds')) {
-    timeout_seconds = 60
-}
-
-if(timeout_seconds in String) {
-    timeout_seconds = Integer.decode(timeout_seconds)
-}
-
-//type check user defined parameters/bindings
-if(!(timeout_seconds in Integer)) {
-    throw new Exception('PARAMETER ERROR: timeout_seconds must be an integer.')
-}
-
-Logger logger = Logger.getLogger('jenkins.instance.restart')
-
-//start a background thread
-def thread = Thread.start {
-    logger.log(Level.INFO, "Jenkins safe restart initiated.")
-    while(true) {
-        if(Jenkins.instance.isQuietingDown()) {
-            if(RestartListener.isAllReady()) {
-                Jenkins.instance.restart()
-            }
-            logger.log(Level.INFO, "Jenkins jobs are not idle.  Waiting ${timeout_seconds} seconds before next restart attempt.")
-            sleep(timeout_seconds*1000)
-        }
-        else {
-            logger.log(Level.INFO, "Shutdown mode not enabled.  Jenkins restart aborted.")
-            break
-        }
-    }
-}
-
-println 'A safe restart has been scheduled.  See the Jenkins logs for restart status updates.  Logger is jenkins.instance.restart.'
