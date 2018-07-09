616163d461e4fa313eb6125b614b7a7c88f24221
skylerto <skylerclayne@gmail.com>
2018-07-09 08:16:02 -0400

    indellient origin
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- plan.sh
+++ plan.sh
@@ -1,5 +1,5 @@
 pkg_name=jenkins
-pkg_origin=skylerto
+pkg_origin=indellient
 pkg_version=2.121.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
