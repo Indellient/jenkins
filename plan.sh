cdc95e49e392577004aa18a3b5bdea5c85e8751a
Jeff Mery <jeff@merys.net>
2018-12-11 14:09:33 -0600

    Fork to jmery

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=jenkins
-pkg_origin=indellient
-pkg_version=2.121.1
+pkg_origin=jmery
+pkg_version=2.138.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('mit')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum="5bb075b81a3929ceada4e960049e37df5f15a1e3cfc9dc24d749858e70b48919"
+pkg_shasum="953e4dda2d3065284c0016b3e8279e097f830c128b1f712d84780ff2b0751e7d"
 pkg_deps=(
   core/jre8
   core/curl
